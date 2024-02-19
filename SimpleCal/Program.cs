4cd177ec6129ff961190e877fb16c68f8550539c
User <User@MOHAMMAD-MALLAH>
2024-02-19 16:54:39 +0200

    Add project files.

---
+++ SimpleCal/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator");
+
+        while (true)
+        {
+            Console.WriteLine("Enter an operation (+, -, *, /) or 'q' to quit:");
+            string operation = Console.ReadLine();
+
+            if (operation == "q")
+                break;
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                        Console.WriteLine("Error: Cannot divide by zero!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation!");
+                    break;
+            }
+
+            Console.WriteLine($"Result: {result}\n");
+        }
+
+        Console.WriteLine("Calculator has been closed. Press any key to exit...");
+        Console.ReadKey();
+    }
+}
+
